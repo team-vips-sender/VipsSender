96d285e5e11f725c4b8fa15d69ba3f77695f9ae7
warabi <warabiwarabi910@gmail.com>
2019-02-03 14:54:15 +0900

    不正な引数のエラーチェック処理を追加

--- src/VipsSender.py
+++ src/VipsSender.py
@@ -1,11 +1,28 @@
 import json
 import sys
 from Vips import Vips
+import Logger
 
-args = sys.argv
-send_info_str = args[1]
 
-send_info = json.loads(send_info_str)
+def __convert_send_info_to_dict(send_info_str):
+    send_info = {}
 
-vips = Vips()
-vips.send(send_info)
+    try:
+        send_info = json.loads(send_info_str)
+    except json.decoder.JSONDecodeError as e:
+        Logger.Logging('VipsSender : Wrong argument format : {0}'.format(e))
+
+    return send_info
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    
+    if len(args) != 2:
+        Logger.Logging('VipsSender : Wrong argument num : {0}'.format(len(args)))
+    else:
+        send_info = __convert_send_info_to_dict(args[1])
+    
+        if send_info:
+            vips = Vips()
+            vips.send(send_info)
